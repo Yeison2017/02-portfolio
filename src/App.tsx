44205d2fdf55c8894bd9b9e9a2813ab5ee599a94
Yeison <yha.big@gmail.com>
2022-12-02 01:55:07 -0500

    Se crea componente Narbar y los estilos para app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Navbar } from "./components";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import "./App.scss";
+
 const App = () => {
-    return <h1>App</h1>;
+    return (
+        <div className="app">
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonial />
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
