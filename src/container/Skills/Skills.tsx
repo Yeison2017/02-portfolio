ecae5415e913778f9697dd854ab71dbc1a51c0cb
yeison2017 <yha.big@gmail.com>
2022-12-25 17:17:38 -0500

    Se crea las interfaces de la entidad Skills

--- src/container/Skills/Skills.tsx
+++ src/container/Skills/Skills.tsx
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import ReactTooltip from "react-tooltip";
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import { WORK } from "../../constants/appCollections";
+import { ISkills } from "../../interfaces";
 import "./Skills.scss";
 
 const Skills = () => {
-    return <div>Skills</div>;
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState<ISkills[]>([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">Skills & Experience</h2>
+
+      <div className="app__skills-container">
+        {skills.map((skill) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className="app__skills-list"
+            key={skill.name}
+          >
+            <div
+              className="app__flex"
+              style={{ backgroundColor: skill.bgColor }}
+            >
+              <img src={urlFor(skill.icon).url()} alt={skill.name} />
+            </div>
+            <div className="p-text">{skill.name}</div>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Skills;
---
+++ src/interfaces/skills.ts
@@ -0,0 +1,5 @@
+export interface ISkills {
+  name: string;
+  bgColor: string;
+  icon: string;
+}
