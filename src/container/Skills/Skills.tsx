1b75b43ca2486f3aadb208a668d84659f7656f77
yeison2017 <yha.big@gmail.com>
2022-12-30 23:46:08 -0500

    Se crea el estilo de Skills

--- src/container/Skills/Skills.tsx
+++ src/container/Skills/Skills.tsx
@@ -5,9 +5,10 @@ import ReactTooltip from "react-tooltip";
 import { urlFor, client } from "../../client";
 import { ISkills, IExperiences } from "../../interfaces";
 import "./Skills.scss";
+import { AppWrap } from "../../wrapper";
 
 const Skills = () => {
-  const [experience, setExperience] = useState<IExperiences[]>([]);
+  const [experiences, setExperiences] = useState<IExperiences[]>([]);
   const [skills, setSkills] = useState<ISkills[]>([]);
 
   useEffect(() => {
@@ -17,7 +18,7 @@ const Skills = () => {
     client.fetch(query).then((data) => {
       console.log(data);
 
-      setExperience(data);
+      setExperiences(data);
     });
 
     client.fetch(skillsQuery).then((data) => {
@@ -27,11 +28,11 @@ const Skills = () => {
 
   return (
     <>
-      <h2 className="head-text">Skills & Experience</h2>
+      <h2 className="head-text">Skills & Experiences</h2>
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills?.map((skill) => (
+          {skills.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
@@ -48,48 +49,35 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-        <motion.div className="app__skills-exp">
-          {experience?.map((experience) => (
-            <>
-              <motion.div
-                className="app__skills-exp-item"
-                key={experience.year}
-              >
-                <div className="app__skills-exp-year">
-                  <p className="bold-text">{experience.year}</p>
-                </div>
-                <motion.div className="app__skills-exp-works">
-                  {experience.works.map((work) => (
-                    <>
-                      <motion.div
-                        whileInView={{ opacity: [0, 1] }}
-                        transition={{ duration: 0.5 }}
-                        className="app__skills-exp-work"
-                        data-tip
-                        data-for={work.name}
-                        key={work.name}
-                      >
-                        <h4 className="bold-text">{work.name}</h4>
-                        <p className="p-text">{work.company}</p>
-                      </motion.div>
-                      {/* <ReactTooltip
-                        id={work.name}
-                        effect="solid"
-                        arrowColor="#fff"
-                        className="skills-tooltip"
-                      >
-                        {work.desc}
-                      </ReactTooltip> */}
-                    </>
-                  ))}
-                </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                  </>
+                ))}
               </motion.div>
-            </>
+            </motion.div>
           ))}
-        </motion.div>
+        </div>
       </div>
     </>
   );
 };
 
-export default Skills;
+export default AppWrap(Skills, "skills");
